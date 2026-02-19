182b42a21671cedc3d26d4b746acf303c6861016
Eddie Makhanu <edwinmakhanu630@gmail.com>
2026-02-19 14:33:02 +0300

    scroll up update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop"; // Import the helper
 import Home from "./components/Home";
 import About from "./components/About";
 import Vision from "./components/Vision";
-import Contact from "./components/Contact"; // 1. Import Contact
-import Support from "./components/Support"; // 2. Import Support
+import Contact from "./components/Contact";
+import Support from "./components/Support";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* 1. Add this here */}
       <Routes>
-        {/* Base URL */}
         <Route path="/" element={<Home />} />
-        
-        {/* About Page */}
         <Route path="/about" element={<About />} />
-
-        {/* Vision Page */}
         <Route path="/vision" element={<Vision />} />
-
-        {/* 3. Added the Contact route */}
         <Route path="/contact" element={<Contact />} />
-
-        {/* 4. Added the Support route */}
         <Route path="/support" element={<Support />} />
       </Routes>
     </Router>
